fb0f207284eb3db0f4d9c8772f7e65d830eeec88
Fransiscus Kevin <81698880+kvnsurya666@users.noreply.github.com>
2021-09-01 20:33:42 +0700

    update last but not least

--- application/views/layout/banner/produk.php
+++ application/views/layout/banner/produk.php
@@ -1,11 +1,11 @@
 <div class="services-breadcrumb_w3ls_agileinfo">
-    <div class="inner_breadcrumb_agileits_w3">
-    	<ul class="short">
-    		<li><a href="<?= base_url() ?>">Home</a><i> | </i></li>
-    		<li><a href="#">Produk</a><i> | </i></li>
-    		<li><?= $kategori ?></li>
-    	</ul>
-    </div>
+	<div class="inner_breadcrumb_agileits_w3">
+		<ul class="short">
+			<li><a href="<?= base_url() ?>">Home</a><i> | </i></li>
+			<li><a href="<?= base_url() ?>">Produk</a><i> | </i></li>
+			<li><?= $kategori ?></li>
+		</ul>
+	</div>
 </div>
 <?= $close_tag ?>
 <?= $close_tag ?>
