9413f312aad867f3556844d2a5231659ce4eed6d
Ade Prisma <adeprisma7@gmail.com>
2021-09-06 09:22:50 +0700

    logo repair

--- application/views/produk.php
+++ application/views/produk.php
@@ -1,3 +1,8 @@
+<?php
+	header("Cache-Control: no-cache, must-revalidate"); // HTTP 1.1
+	header("Pragma: no-cache"); // HTTP 1.0
+	header("Expires: Wed, 1 Jan 2020 00:00:00 GMT"); // Anytime in the past
+?>
 <div class="ads-grid_shop">
 
 	<!---728x90--->
